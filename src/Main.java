b1ef369b7d41c1e58615500fca59ce99fcfa4078
asrytikov <asrytikov@gmail.com>
2023-09-20 10:34:54 +0400

    Lesson2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,102 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // jhgfhjdghfjgsdjh; gsdfghsfj;nbshndfgvgdhs
+        /*dshfghsdgfjhdgs
+        * shdjfsd
+        * sdfhjsdhfs'
+        * dfhsdjfkds'sdjhkfh
+        * sdjhfsd*/
+        /*System.out.println("Hello world!");
+        System.out.println("Hello world!");*/
+        System.out.println("Hello world! 3"); //hgfghdfghgf
+        System.out.println("Hello world! 4");
+
+        //тип данных имя_переменной;
+        int x;
+        int X = 20;
+
+        x=10;
+
+        int y = 100;
+        System.out.println(X);
+
+        int x1=10, x2=20, x3;
+        x1 =    20;
+        System.out.println(x1);
+        x1 = x2;
+
+        var x10 = 100.10;
+
+        final int LIMIT = 10;
+
+        int summaZarplatProgrammistov;
+
+        boolean isActive = false;
+        boolean isAlive = true;
+
+        byte a=3; //-128 .. 127 1 байт
+        short b=88; // -32768 .. 32767 2 байта
+        int c = 100; // -2*10^9 - 2*10^9 4 байта
+        long d = 1000; // -9 *10^18 - 9*10^18 8 байт
+
+        double f = 10000.001; // 5*10^-324 - 1.8*10^308 8 байт
+        float h = 10000.001F; // -3 *10^38 - 3*10^38 4 байта
+
+        char r = 'r'; //2 байта UTF-16
+
+        long gh = 9999999999L;
+
+        int xz = 0x6F; //111
+        int num8 = 010; //8
+        int num2 = 0b1101;//13
+        int t = 123_463;
+        System.out.println(t);
+
+        System.out.println(r);
+
+        String str = "shdfghjsdghfj";
+        String r2 = "r";
+
+        char a1 = 'a';
+        char b1 = 'b';
+        char c22 = (char)(a1+b1);
+        System.out.println(a1+b1);
+
+        String v1 = "a";
+        String v2 = "b";
+        System.out.println(v1+v2);
+
+        char ch = '\u0066'; //ACSII
+        System.out.println(ch);
+
+        char ch1 = 122;
+
+
+        //"\n"
+        String text = "Hello \n all";
+        System.out.println(text);
+
+        String t1 = "Hello all! \n" +
+                "Java is the most popular language\n"+
+                "Java is the best!";
+
+        String t2 = """
+                Hello all!
+                Java is the most popular language
+                Java is the best!
+                """;
+
+        System.out.println(t1);
+        System.out.println("---------------");
+        System.out.println(t2);
+
+
+
+
+
+
+
+
+
     }
 }
